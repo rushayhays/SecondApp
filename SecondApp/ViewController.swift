5a089886931d58a72a5d52100680016c251300c6
rushayhays <rushayhays@outlook.com>
2022-09-14 17:12:38 -0400

    Initial Commit

---
+++ SecondApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondApp
+//
+//  Created by Sarah Hays on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
